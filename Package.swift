960ef1f454ebc5e18a97acb38cebe9a4786e1149
Attila Bencze <attila.bencze@intopalo.com>
2017-07-05 09:31:15 +0300

    Change git protocol in dependencies to https

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         Target(name: "AppIconSetGen", dependencies: ["AppIconSetGenCore"]),
     ],
     dependencies: [
-        .Package(url: "git@github.com:kylef/Commander.git", majorVersion: 0),
-        .Package(url: "git@github.com:kylef/PathKit.git", majorVersion: 0),
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0),
     ]
 )
